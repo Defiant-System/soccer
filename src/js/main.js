c7a53863a44fd2b8e946bf81769fc730be7ab981
Hakan Bilgin <hbi99@hotmail.com>
2025-06-17 12:29:06 +0200

    arena ui

--- src/js/main.js
+++ src/js/main.js
@@ -7,21 +7,54 @@ const football = {
 		// fast references
 		this.content = window.find("content");
 
+		// init all sub-objects
+		Object.keys(this)
+			.filter(i => typeof this[i].init === "function")
+			.map(i => this[i].init(this));
+
 		// DEV-ONLY-START
 		Test.init(this);
 		// DEV-ONLY-END
 	},
 	dispatch(event) {
+		let Self = football,
+			el;
 		switch (event.type) {
 			// system events
 			case "window.init":
+				break;
+			case "window.keystroke":
+				switch (event.char) {
+					case "w":
+					case "s":
+					case "a":
+					case "d":
+					case "up":
+					case "down":
+					case "left":
+					case "right":
+						Self.game.dispatch(event);
+						break;
+				}
 				break;
 			// custom events
 			case "open-help":
 				karaqu.shell("fs -u '~/help/index.md'");
 				break;
+			// proxy events
+			default:
+				el = event.el;
+				if (!el && event.origin) el = event.origin.el;
+				if (el) {
+					let pEl = el.parents(`?div[data-area]`);
+					if (pEl.length) {
+						let name = pEl.data("area");
+						return Self[name].dispatch(event);
+					}
+				}
 		}
-	}
+	},
+	game: @import "./areas/game.js",
 };
 
 window.exports = football;
