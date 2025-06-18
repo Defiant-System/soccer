24191f9875dba4ad6cf36d6780ccada3fdfc568b
Hakan Bilgin <hbi99@hotmail.com>
2025-06-19 00:30:24 +0200

    Update ball.js

--- src/js/classes/ball.js
+++ src/js/classes/ball.js
@@ -28,9 +28,22 @@ class Ball {
 		let w = 32,
 			f = this.frame.index * w,
 			x = 100,
-			y = 100;
+			y = 100,
+			r = 15,
+			gx = x+r,
+			gy = y+r,
+			gradient = ctx.createRadialGradient(gx-3, gy-3, 0, gx, gy, r);
+		// 3d roundness
+		gradient.addColorStop(0.0, "#fff7");
+		gradient.addColorStop(0.7, "#1115");
+		gradient.addColorStop(1.0, "#1119");
+
 		ctx.save();
 		ctx.drawImage(this.asset.img, f, 0, w, w, x, y, w, w);
+		ctx.fillStyle = gradient;
+		ctx.beginPath();
+		ctx.arc(gx+1, gy+1, r-1, 0, Math.TAU);
+		ctx.fill();
 		ctx.restore();
 	}
 }
